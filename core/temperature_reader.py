dc4f42f3065791947498d755e086f5dbfcceace9
mrj <m@badcrumble.net>
2021-01-23 15:20:04 +0000

    Fix mistake in except clause

--- core/temperature_reader.py
+++ core/temperature_reader.py
@@ -43,7 +43,7 @@ class TemperatureReader:
                 try:
                     value = float(raw_value)
                 except ValueError:
-                    sys.stderr.write("Couldn't parse '" + rawvalue + "'\n")
+                    sys.stderr.write("Couldn't parse '" + raw_value + "'\n")
                 else:
                     lock.acquire()
                     self.value = value / 1000
